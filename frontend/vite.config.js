0b667414ba4b363d6f352463d027412343f8a540
StewardMina <sminacosme@gmail.com>
2026-03-25 16:57:10 -0500

    Configure Vite to serve ley1620.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        ley1620: 'public/ley1620.html'
+      }
+    }
+  }
 })
